7b18213d01f5adccc1b9b88e00ea27149fe406b4
nanphg287 <120617395+nanphg287@users.noreply.github.com>
2023-02-22 10:17:02 +0700

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class hello
+{
+    public static void Main(string[] args)
+    {
+        //WriteLine(): method writes the current line terminator to the standart output stream
+        Console.WriteLine("Hello");
+        Console.Write("Nanphghihi");
+        Console.Write(" she is not real");
+
+        Console.ReadLine();
+    }
+}
