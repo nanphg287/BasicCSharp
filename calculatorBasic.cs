fabf8f1212c8a38f5a4469e56c112c77f27ca08d
nanphg287 <120617395+nanphg287@users.noreply.github.com>
2023-02-22 10:27:11 +0700

    Add files via upload

---
+++ calculatorBasic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleTasks
+{
+    internal class calculatorBasic
+    {
+        public static void Main(string[] args)
+        {
+            // task: calculator basic
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+
+        }
+    }
+    
+}
