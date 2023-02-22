a4aaf88e7c31143cef6f96b855e8d05e7a497303
nanphg287 <120617395+nanphg287@users.noreply.github.com>
2023-02-22 11:08:12 +0700

    Add files via upload

---
+++ differenceBtwTwoDates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleTasks
+{
+    internal class differenceBtwTwoDates
+    {
+        public static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine(currentDate.ToString("dd/MM/yyyy"));
+
+            DateTime date1 = new DateTime(2023,07,28);
+            Console.WriteLine(date1.ToString("dd/MM/yyyy"));
+
+            double diff = date1.Subtract(currentDate).TotalDays; 
+
+            Console.WriteLine("Difference between dates (number of days): " + Math.Round(diff));
+
+        }
+    }
+}
